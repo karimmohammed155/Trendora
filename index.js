c75f19ac4a441ae105719363989610dded08c78c
unknown <kemomohammed155@gmail.com>
2025-09-27 17:41:05 +0300

    last update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import OperationRouter from './src/modules/OperationDepartment/operationRouter.j
 import dashBoardRouter from './src/modules/dashboard/dashboardRouter.js';
 import cors from 'cors';
 dotenv.config();
-await connectDB();
+ connectDB();
 const app = express()
 const port = process.env.PORT || 3000;
 
