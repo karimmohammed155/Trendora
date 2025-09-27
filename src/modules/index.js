2241f36140edcb3874c057aaae38eaa7dccd5719
unknown <kemomohammed155@gmail.com>
2025-09-27 13:23:17 +0300

    last update

---
+++ src/modules/index.js
@@ -0,0 +1,6 @@
+export * from "./HRDepartment/HRDeptRouter.js";
+export * from "./ITDepartment/ITDeptRouter.js";
+export * from "./OperationDepartment/operationRouter.js";
+export * from "./dashboard/dashboardRouter.js";
+export * from "./employee_department/employee.route.js";
+export * from "./user/user.route.js";
