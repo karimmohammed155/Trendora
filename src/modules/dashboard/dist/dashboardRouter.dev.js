7897e7be23b889020c7b675f293868da91f8599b
Yara Sayed <yara.yarasayed@gmail.com>
2025-09-29 12:10:56 +0300

    first commit

---
+++ src/modules/dashboard/dist/dashboardRouter.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = require("express");
+
+var _authorizationMiddleware = require("../../middlewares/authorization.middleware.js");
+
+var _dashboardSchema = require("./dashboardSchema.js");
+
+var _validationMiddleware = require("../../middlewares/validationMiddleware.js");
+
+var _dashboardController = require("./dashboardController.js");
+
+var router = (0, _express.Router)(); //Add leave
+
+router.post('/leaves', (0, _validationMiddleware.validation)(_dashboardSchema.addLeaveSchema), _dashboardController.addLeave); //Add ticket
+
+router.post('/tickets', (0, _validationMiddleware.validation)(_dashboardSchema.addTicketSchema), _dashboardController.addTicket);
+var _default = router;
+exports["default"] = _default;
