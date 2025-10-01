811beb0f8b98421917d3e174eeff20a6008e3a3d
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-01 10:26:43 +0300

    added pagination to operation and digital marketing pages

--- src/modules/OperationDepartment/operationController.js
+++ src/modules/OperationDepartment/operationController.js
@@ -44,7 +44,11 @@ export const addCampaign=asyncHandler(async(req,res,next)=>{
 
 //get all campaigns
 export const getAllCampaigns=asyncHandler(async(req,res,next)=>{
-    const campaigns=await Campaign.find();
+        const page= parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const campaigns=await Campaign.find().skip(skip).limit(limit);
     if(campaigns.length===0){
         return next(new Error("No campaigns found",{cause:404}));
     }
