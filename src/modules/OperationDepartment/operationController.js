801fff92d4feb80de97f653c196407b3f54f8107
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-09 11:07:16 +0300

    adjusted pagination

--- src/modules/OperationDepartment/operationController.js
+++ src/modules/OperationDepartment/operationController.js
@@ -44,7 +44,7 @@ export const addCampaign=asyncHandler(async(req,res,next)=>{
 
 //get all campaigns
 export const getAllCampaigns=asyncHandler(async(req,res,next)=>{
-        const page= parseInt(req.query.page) || 1;
+    const page= parseInt(req.query.page) || 1;
     const limit = parseInt(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
