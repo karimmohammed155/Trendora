be32f080a65c59dfc6c22ce717540438e81a174e
unknown <kemomohammed155@gmail.com>
2025-10-09 13:16:03 +0300

    last update

---
+++ DB/models/Transaction.model.js
@@ -0,0 +1,15 @@
+import mongoose from "../global-setup.js";
+const { model, Schema } = mongoose;
+
+const transaction_schema = new Schema(
+  {
+    description: { type: String, required: true },
+    amount: { type: Number, required: true },
+    date: { type: String, required: true },
+    type: { type: String, enum: ["income", "expense"], required: true },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const transaction =
+  mongoose.models.transaction || model("transaction", transaction_schema);
