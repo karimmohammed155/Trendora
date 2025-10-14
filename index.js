ba8219bfdcc71db060f6dbf22b5684814883765a
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-14 10:40:50 +0300

    final touches

--- index.js
+++ index.js
@@ -29,14 +29,6 @@ app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files (React build)
-app.use(express.static(path.join(__dirname, "dist"))); // or "dist" if using Vite
-
-// Fallback for SPA routing
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
 app.use(
   "/api/digitalMarketing",
   auth(),
@@ -64,6 +56,15 @@ app.all("/{*any}", (req, res, next) => {
 });
 
 app.use(global_response);
+
+// Serve static files (React build)
+app.use(express.static(path.join(__dirname, "dist"))); // or "dist" if using Vite
+
+// Fallback for SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.use((error, req, res, next) => {
   const statusCode = error.cause || 500;
 
