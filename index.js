cefe58190e79284d3919bd63fdb56f283e3109d8
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-14 10:36:50 +0300

    final touches

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ import { global_response } from "./src/middlewares/error.handle.middleware.js";
 import { authorizeDepartment } from "./src/middlewares/authorizeDepartment.js";
 import { auth } from "./src/middlewares/auth_middleware.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files (React build)
+app.use(express.static(path.join(__dirname, "dist"))); // or "dist" if using Vite
+
+// Fallback for SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 dotenv.config();
 await connectDB();
 const app = express();
@@ -23,13 +37,28 @@ app.use(express.json());
 // Enable CORS for all routes
 app.use(cors());
 
-app.use("/api/digitalMarketing",auth(),authorizeDepartment('Digital Marketing'), digitalMarketingRouter);
+app.use(
+  "/api/digitalMarketing",
+  auth(),
+  authorizeDepartment("Digital Marketing"),
+  digitalMarketingRouter
+);
 app.use("/api/user", user_router);
-app.use("/api/hr",auth(),authorizeDepartment('HR'), HRDeptRouter);
-app.use("/api/it",auth(),authorizeDepartment('IT'), ITDeptRouter);
-app.use("/api/operation",auth(),authorizeDepartment('Operation'), OperationRouter);
+app.use("/api/hr", auth(), authorizeDepartment("HR"), HRDeptRouter);
+app.use("/api/it", auth(), authorizeDepartment("IT"), ITDeptRouter);
+app.use(
+  "/api/operation",
+  auth(),
+  authorizeDepartment("Operation"),
+  OperationRouter
+);
 app.use("/api/dashboard", dashBoardRouter);
-app.use("/api/accounting",auth(),authorizeDepartment('Accounting'), accounting_router);
+app.use(
+  "/api/accounting",
+  auth(),
+  authorizeDepartment("Accounting"),
+  accounting_router
+);
 app.all("/{*any}", (req, res, next) => {
   return next(new Error("Page not found", { cause: 404 }));
 });
