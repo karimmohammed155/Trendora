48b0d3033ca1aef32c35ea532a09770016fdf595
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-30 13:59:20 +0300

    no more errors in the console

--- DB/models/projectsModel.js
+++ DB/models/projectsModel.js
@@ -1,19 +1,22 @@
 import mongoose, { model, Schema, Types } from "mongoose";
 
-const ProjectSchema = new Schema({
-  name: { type: String, required: true }, // Project title
-  description: { type: String,required:true }, // Summary of project
-  customerName:{ type: String,trim: true  }, // Client or customer name
-  status: { 
-    type: String, 
-    enum: ["planned", "in_progress", "on_hold", "completed"], 
-    default: "planned" 
+const ProjectSchema = new Schema(
+  {
+    name: { type: String, required: true }, // Project title
+    description: { type: String }, // Summary of project
+    customerName: { type: String, trim: true }, // Client or customer name
+    status: {
+      type: String,
+      enum: ["planned", "in_progress", "on_hold", "completed"],
+      default: "planned",
+    },
+    department: { type: Types.ObjectId, ref: "Department" },
+    members: [{ type: Types.ObjectId, ref: "Employee" }], // team members
+    notes: { type: String }, // general notes / remarks
+    startDate: { type: Date },
+    endDate: { type: Date },
   },
-  department: { type: Types.ObjectId, ref: "Department" },
-  members: [{ type: Types.ObjectId, ref: "Employee" }], // team members
-  notes: { type: String }, // general notes / remarks
-  startDate: { type: Date },
-  endDate: { type: Date },
-}, { timestamps: true ,versionKey:false});
+  { timestamps: true, versionKey: false }
+);
 
 export const Project = model("Project", ProjectSchema);
