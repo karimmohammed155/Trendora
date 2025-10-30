6504e405adb11e59ddac1c4018a760675a18b262
Yara Sayed <yara.yarasayed@gmail.com>
2025-10-30 14:08:18 +0300

    no more errors in the console

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -19,7 +19,7 @@ export const log_in = async (req, res, next) => {
     return next(
       new Error_handler_class(
         "your account is inactive, please contact admin",
-        400,
+        403,
         "login api"
       )
     );
