e51c30b46bc8ec52e2bc159ec3b4149c272f3434
Yara Sayed <yara.yarasayed@gmail.com>
2025-11-03 12:36:35 +0200

    no more errors in the console

--- DB/models/leavesModel.js
+++ DB/models/leavesModel.js
@@ -1,12 +1,19 @@
-import mongoose,{ model, Schema, Types } from "mongoose"; 
+import mongoose, { model, Schema, Types } from "mongoose";
 
- 
-const LeaveSchema = new Schema({
-  employee: { type: Types.ObjectId, ref: "Employee", required: true },
-  startDate: Date,
-  endDate: Date,
-  type: { type: String, enum: ["annual", "sick", "unpaid"], required: true },
-  status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" }
-}, { timestamps: true, versionKey: false });
+const LeaveSchema = new Schema(
+  {
+    employee: { type: Types.ObjectId, ref: "Employee", required: true },
+    startDate: Date,
+    endDate: Date,
+    type: { type: String, enum: ["annual", "sick", "unpaid"], required: true },
+
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
 
 export const Leave = model("Leave", LeaveSchema);
